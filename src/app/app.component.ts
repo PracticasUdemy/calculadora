b4ce4890b950b03db797f8effe32a974f6d418ab
John Castañeda <bayron410@gmail.com>
2022-01-30 13:17:02 -0500

    Modularizando las partes de la calculadora (resultado, formulario)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Aplicación Calculadora';
-  operandoA: number = 0;
-  operandoB: number = 0;
   resultado: number = 0;
 
-  sumar(): void {
-    this.resultado = this.operandoA + this.operandoB;
+  mostrarResultado(resultado: number) {
+    this.resultado = resultado;
   }
 }
